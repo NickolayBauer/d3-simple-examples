c184edb72c3edbdb80e97d18a6ad686a19daa5d0
nickolas <kolanlos510@gmail.com>
2020-02-24 16:59:35 +0300

    histogramm

--- src/views/Figures/Histogram.vue
+++ src/views/Figures/Histogram.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="Histogram">
-        <svg style="width:100vh; height: 60vh;"/>
+        <svg style="width:50vh; height: 50vh;" />
     </div>
 </template>
 <script>
@@ -14,34 +14,40 @@ export default {
         };
     },
     mounted() {
-        const data = this.cities.sort(function(b, a) {
-            return a.population - b.population;
-        });
+        this.render();
+    },
+    methods: {
+        render() {
+            const data = this.cities.sort(function(b, a) {
+                return a.population - b.population;
+            });
 
-        const svg = select("svg");
+            const svg = select("svg")
 
-        const width = 520;
-        const height = 200;
+            const width = window.innerWidth * 0.6;
+            const height = 200;
+            console.log(width);
 
-        const xValue = d => d.population;
-        const yValue = d => d.city;
+            const xValue = d => d.population;
+            const yValue = d => d.city;
 
-        const xScale = scaleLinear()
-            .domain([0, max(data, xValue)])
-            .range([0, width]);
+            const xScale = scaleLinear()
+                .domain([0, max(data, xValue)])
+                .range([0, width]);
 
-        const yScale = scaleBand()
-            .domain(data.map(yValue))
-            .range([0, height])
-            .padding(0.1);
+            const yScale = scaleBand()
+                .domain(data.map(yValue))
+                .range([0, height])
+                .padding(0.1);
 
-        svg.selectAll("rect")
-            .data(data)
-            .enter()
-            .append("rect")
-            .attr("y", d => yScale(yValue(d)))
-            .attr("width", d => xScale(d.population))
-            .attr("height", yScale.bandwidth());
+            svg.selectAll("rect")
+                .data(data)
+                .enter()
+                .append("rect")
+                .attr("y", d => yScale(yValue(d)))
+                .attr("width", d => xScale(d.population))
+                .attr("height", yScale.bandwidth())
+        },
     }
 };
 </script>
